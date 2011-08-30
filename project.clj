4a805f67ba4f32df62fd3e663d58732f3e460adf
Allen Rohner <arohner@gmail.com>
2011-08-29 21:18:06 -0500

    Bump the version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-table "0.1.3"
+(defproject clj-table "0.1.4"
   :description "a SQL ORM for clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
